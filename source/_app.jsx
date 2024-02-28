8bbf1e24dac2a2cb0eca8ba76a7939b8575fe753
Felipe Oliveira <felipe.oliveira@gympass.com>
2024-02-28 09:59:35 -0300

    feat: add the react setup

---
+++ source/_app.jsx
@@ -0,0 +1,11 @@
+import './global.scss'
+
+import Home from './pages/Home'
+
+export default function App() {
+	return (
+		<>
+			<Home />
+		</>
+	)
+}
---
+++ source/index.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import App from './_app'
+
+const root = createRoot(document.getElementById('app'))
+
+root.render(<App />)
